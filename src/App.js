1c653356ad9a8d99c895f08e2f50febaaefa7d8d
DaelsFlowers <danieel.flowers@gmail.com>
2022-09-21 12:54:56 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './pages/Home/Home.js';
-import Main from "./pages/Main/Main"
+// import Main from "./pages/Main/Main"
 function App() {
   return (
     <div className="App">
